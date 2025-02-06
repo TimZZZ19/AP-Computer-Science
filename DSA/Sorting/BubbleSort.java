4d8eaf4515d941eaf8d3a5bec6b0e673d7e1b5e5
Tim <tim.email@example.com>
2025-02-06 16:25:31 +0800

    second commit: added bubble sort

---
+++ DSA/Sorting/BubbleSort.java
@@ -0,0 +1,50 @@
+package DSA.Sorting;
+
+public class BubbleSort {
+    // Bubble Sort function
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Last i elements are already sorted
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // If no swapping happened, array is already sorted
+            if (!swapped) break;
+        }
+    }
+
+    // Function to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr)
+            System.out.print(num + " ");
+
+        System.out.println();
+    }
+
+    // Main method to test the sorting
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
+
